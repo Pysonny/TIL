ab46ba2e4edd4ab7e7d26dfb23b8b97033d622be
CHOI GYESU <dwlsc0304@naver.com>
2023-01-05 09:32:14 +0900

    내용 정리

---
+++ pr codes/codes.py
@@ -0,0 +1,19 @@
+1. 리스트에 저장된 정수들의 합
+
+number_list = [ 1,2,3,4,5 ]
+a = 0
+for num in number_list:
+    a += num
+print(a)
+
+
+2. 리스트 요소의 개수 출력
+
+number_list = [ 1,2,3,4,5 ]
+a = 0
+for num in number_list:
+    a += 1
+print(a)
+
+
+3. 
